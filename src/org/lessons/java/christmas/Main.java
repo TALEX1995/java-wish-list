63e9abc3fd7b27bf3690c5933fb7c1e3c9bfbd51
TALEX1995 <tanase.alex@libero.it>
2023-11-17 11:54:11 +0100

    first commit

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,19 @@
+package org.lessons.java.christmas;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		List<String> presents = new ArrayList<>();
+		
+		
+	}
+
+}
